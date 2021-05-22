b24aca088e4a5869b61c72a7269ce6ca44f47995
juablaz <jc-blazquez@hotmail.com>
2021-05-22 11:14:36 +0200

    Pruebas

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # p3-serverless
 Ptractice 3 of Serverless
+
+1. Create a folder wherever you want the project to be and go inside it.
+
+2. Go to console and, in the previous folder route, write the following command to clone this repository (you can download it manually too):
+
+    ```
+    git clone https://github.com/codeurjc-students/2019-ServerlessVsExpress.git
+    ```
+    
+3. From the console, navigate to the folder **"sections/1-REST/aws-lambda"**.
+
+4. Build and deploy
+    ```
+    sam build
+    sam deploy --guided
+    ```
---
+++ aws/src/dbManager.js
@@ -0,0 +1,76 @@
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    endpoint: "https://dynamodb.us-east-1.amazonaws.com",
+    region: "us-east-1"
+});
+
+const docClient = new AWS.DynamoDB.DocumentClient();
+const table = 'users';
+
+// This is a DB simulation. Data should be managed with a real database inside functions.
+
+const getAllUsers = () => {
+    const params = {
+        TableName: table
+    };
+
+    return docClient.scan(params).promise();
+};
+
+const addUser = (data) => {
+    const params = {
+        TableName: table,
+        Item: {
+            "userid": uuid.v1(),
+            "nick": data.nick,
+            "email": data.email,
+        }
+    };
+
+    return docClient.put(params).promise();
+};
+
+const updateUser = (data) => {
+    const params = {
+        TableName: table,
+        Key: {
+            "userid": data.userid
+        },
+        UpdateExpression: "set #na = :n, email = :e",
+        ExpressionAttributeNames: { // Used when there are reserved words in DynamoDB, like name
+            "#na": 'nick'
+        },
+        ExpressionAttributeValues: {
+            ":n": data.nick,
+            ":e": data.email
+        },
+        ReturnValues: "ALL_OLD" // Returns the item content before it was updated
+    };
+
+    return docClient.update(params).promise();
+};
+
+const deleteUser = (userid) => {
+    const params = {
+        TableName: table,
+        Key: {
+            "userid": userid
+        },
+        ConditionExpression: "userid = :userid",
+        ExpressionAttributeValues: {
+            ":userid": userid
+        },
+        ReturnValues: "ALL_OLD" // Returns the item content before it was deleted
+    };
+
+    return docClient.delete(params).promise();
+};
+
+module.exports = {
+    getAllUsers,
+    addUser,
+    updateUser,
+    deleteUser
+};
