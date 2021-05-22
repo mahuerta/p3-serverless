e80551def8dbc171892ca1d675b8e1ea6d9c22a9
mahuerta <miguel5692@gmail.com>
2021-05-22 13:22:16 +0200

    merge

--- aws/.aws-sam/build/usersFunction/dbManager.js
+++ aws/.aws-sam/build/usersFunction/dbManager.js
@@ -9,8 +9,7 @@ AWS.config.update({
 const docClient = new AWS.DynamoDB.DocumentClient();
 const table = 'users';
 
-// This is a DB simulation. Data should be managed with a real database inside functions.
-
+// USERS
 const getAllUsers = () => {
     const params = {
         TableName: table
@@ -26,8 +25,7 @@ const addUser = (data) => {
             "userid": uuid.v1(),
             "nick": data.nick,
             "email": data.email,
-        },
-        ReturnValues: "UPDATED_OLD"
+        }
     };
 
     return docClient.put(params).promise();
@@ -81,10 +79,93 @@ const deleteUser = (userid) => {
     return docClient.delete(params).promise();
 };
 
+
+// BOOKS
+const getAllBooks = () => {
+    const params = {
+        TableName: table
+    };
+
+    return docClient.scan(params).promise();
+};
+
+const addBook = (data) => {
+    const params = {
+        TableName: table,
+        Item: {
+            "bookid": uuid.v1(),
+            "author": data.author,
+            "title": data.title,
+            "summary": data.summary,
+            "publisher": data.publisher,
+            "publicationYear": data.publicationYear
+        }
+    };
+
+    return docClient.put(params).promise();
+};
+
+const getBook = (bookid) => {
+    const params = {
+        TableName: table,
+        Key: {
+            "bookid": bookid
+        },
+        ConditionExpression: "bookid = :bookid",
+        ExpressionAttributeValues: {
+            ":bookid": bookid
+        },
+        ReturnValues: "ALL_NEW"
+    };
+
+    return docClient.get(params).promise();
+};
+
+const updateBook = (data) => {
+    const params = {
+        TableName: table,
+        Key: {
+            "bookid": data.bookid
+        },
+        UpdateExpression: "set author = :author, title = :title, summary = :summary, publisher = :publisher, publicationYear = :publicationYear",
+        ExpressionAttributeValues: {
+            ":author": data.author,
+            ":title": data.title,
+            ":summary": data.summary,
+            ":publisher": data.publisher,
+            ":publicationYear": data.publicationYear
+        },
+        ReturnValues: "ALL_NEW" 
+    };
+
+    return docClient.update(params).promise();
+};
+
+const deleteBook = (bookid) => {
+    const params = {
+        TableName: table,
+        Key: {
+            "bookid": bookid
+        },
+        ConditionExpression: "bookid = :bookid",
+        ExpressionAttributeValues: {
+            ":bookid": bookid
+        },
+        ReturnValues: "ALL_OLD" 
+    };
+
+    return docClient.delete(params).promise();
+};
+
 module.exports = {
     getAllUsers,
     addUser,
     updateUser,
     deleteUser,
-    getUser
+    getUser,
+    getAllBooks,
+    addBook,
+    updateBook,
+    deleteBook,
+    getBook
 };
